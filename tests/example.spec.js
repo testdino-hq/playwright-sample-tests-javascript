f0340f877444ca183e2146b6d412c4a10113e8f3
TestDino <testdinostart@gmail.com>
2025-09-20 10:47:30 +0530

    Merge branch 'main' into prod

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -33,69 +33,69 @@ test('Verify that user can login and logout successfully', async () => {
   await logout();
 });
 
-test.skip('Verify that user can update personal information', async () => {
-  await login();
-  await allPages.userPage.clickOnUserProfileIcon();
-  await allPages.userPage.updatePersonalInfo();
-  await allPages.userPage.verifyPersonalInfoUpdated();
-});
+// test.skip('Verify that user can update personal information', async () => {
+//   await login();
+//   await allPages.userPage.clickOnUserProfileIcon();
+//   await allPages.userPage.updatePersonalInfo();
+//   await allPages.userPage.verifyPersonalInfoUpdated();
+// });
 
-test.skip('Verify that User Can Add, Edit, and Delete Addresses after Logging In', async () => {
-    await login();
+// test.skip('Verify that User Can Add, Edit, and Delete Addresses after Logging In', async () => {
+//     await login();
 
-  await test.step('Verify that user is able to add address successfully', async () => {
-    await allPages.userPage.clickOnUserProfileIcon();
-    await allPages.userPage.clickOnAddressTab();
-    await allPages.userPage.clickOnAddAddressButton();
-    await allPages.userPage.fillAddressForm();
-    await allPages.userPage.verifytheAddressIsAdded();
-  });
+//   await test.step('Verify that user is able to add address successfully', async () => {
+//     await allPages.userPage.clickOnUserProfileIcon();
+//     await allPages.userPage.clickOnAddressTab();
+//     await allPages.userPage.clickOnAddAddressButton();
+//     await allPages.userPage.fillAddressForm();
+//     await allPages.userPage.verifytheAddressIsAdded();
+//   });
 
-  await test.step('Verify that user is able to edit address successfully', async () => {
-    await allPages.userPage.clickOnEditAddressButton();
-    await allPages.userPage.updateAddressForm();
-    await allPages.userPage.verifytheUpdatedAddressIsAdded();
-  })
+//   await test.step('Verify that user is able to edit address successfully', async () => {
+//     await allPages.userPage.clickOnEditAddressButton();
+//     await allPages.userPage.updateAddressForm();
+//     await allPages.userPage.verifytheUpdatedAddressIsAdded();
+//   })
 
-  await test.step('Verify that user is able to delete address successfully', async () => {
-    await allPages.userPage.clickOnDeleteAddressButton();
-  });
-});
+//   await test.step('Verify that user is able to delete address successfully', async () => {
+//     await allPages.userPage.clickOnDeleteAddressButton();
+//   });
+// });
 
-test.skip('Verify that user can change password successfully', async () => {
-  await test.step('Login with existing password', async () => {
-    await login1();
-  });
+// test.skip('Verify that user can change password successfully', async () => {
+//   await test.step('Login with existing password', async () => {
+//     await login1();
+//   });
 
-  await test.step('Change password and verify login with new password', async () => {
-    await allPages.userPage.clickOnUserProfileIcon();
-    await allPages.userPage.clickOnSecurityButton();
-    await allPages.userPage.enterNewPassword();
-    await allPages.userPage.enterConfirmNewPassword();
-    await allPages.userPage.clickOnUpdatePasswordButton();
-    await allPages.userPage.getUpdatePasswordNotification();
-  });
-  await test.step('Verify login with new password and revert back to original password', async () => {
-    // Re-login with new password
-    await logout();
-    await allPages.loginPage.login(process.env.USERNAME1, process.env.NEW_PASSWORD);
+//   await test.step('Change password and verify login with new password', async () => {
+//     await allPages.userPage.clickOnUserProfileIcon();
+//     await allPages.userPage.clickOnSecurityButton();
+//     await allPages.userPage.enterNewPassword();
+//     await allPages.userPage.enterConfirmNewPassword();
+//     await allPages.userPage.clickOnUpdatePasswordButton();
+//     await allPages.userPage.getUpdatePasswordNotification();
+//   });
+//   await test.step('Verify login with new password and revert back to original password', async () => {
+//     // Re-login with new password
+//     await logout();
+//     await allPages.loginPage.login(process.env.USERNAME1, process.env.NEW_PASSWORD);
 
-    // Revert back
-    await allPages.userPage.clickOnUserProfileIcon();
-    await allPages.userPage.clickOnSecurityButton();
-    await allPages.userPage.revertPasswordBackToOriginal();
-    await allPages.userPage.getUpdatePasswordNotification();
-  })
-});
+//     // Revert back
+//     await allPages.userPage.clickOnUserProfileIcon();
+//     await allPages.userPage.clickOnSecurityButton();
+//     await allPages.userPage.revertPasswordBackToOriginal();
+//     await allPages.userPage.getUpdatePasswordNotification();
+//   })
+// });
 
-test.skip('Verify that the New User is able to add Addresses in the Address section', async () => {
-  await login();
-  await allPages.userPage.clickOnUserProfileIcon();
-  await allPages.userPage.clickOnAddressTab();
-  await allPages.userPage.clickOnAddAddressButton();
-  await allPages.userPage.checkAddNewAddressMenu();
-  await allPages.userPage.fillAddressForm();
-});
+// test.skip('Verify that the New User is able to add Addresses in the Address section', async () => {
+//   await login();
+//   await allPages.userPage.clickOnUserProfileIcon();
+//   await allPages.userPage.clickOnAddressTab();
+//   await allPages.userPage.clickOnAddAddressButton();
+//   await allPages.userPage.checkAddNewAddressMenu();
+//   await allPages.userPage.fillAddressForm();
+// });
 
 test('Verify that User Can Complete the Journey from Login to Order Placement', async () => {
   const productName = 'GoPro HERO10 Black';
@@ -117,63 +117,63 @@ test('Verify that User Can Complete the Journey from Login to Order Placement', 
   await allPages.checkoutPage.verifyOrderPlacedSuccessfully();
 });
 
-test.skip('Verify user can place and cancel an order', async () => {
-  const productName = 'GoPro HERO10 Black';
-  const productPriceAndQuantity = '₹49,999 × 1';
-  const productQuantity = '1';
-  const orderStatusProcessing = 'Processing';
-  const orderStatusCanceled = 'Canceled';
+// test.skip('Verify user can place and cancel an order', async () => {
+//   const productName = 'GoPro HERO10 Black';
+//   const productPriceAndQuantity = '₹49,999 × 1';
+//   const productQuantity = '1';
+//   const orderStatusProcessing = 'Processing';
+//   const orderStatusCanceled = 'Canceled';
 
-  await test.step('Verify that user can login successfully', async () => {
-    await login();
-    await allPages.inventoryPage.clickOnAllProductsLink();
-    await allPages.inventoryPage.searchProduct(productName);
-    await allPages.inventoryPage.verifyProductTitleVisible(productName);
-    await allPages.inventoryPage.clickOnAddToCartIcon();
-  })
+//   await test.step('Verify that user can login successfully', async () => {
+//     await login();
+//     await allPages.inventoryPage.clickOnAllProductsLink();
+//     await allPages.inventoryPage.searchProduct(productName);
+//     await allPages.inventoryPage.verifyProductTitleVisible(productName);
+//     await allPages.inventoryPage.clickOnAddToCartIcon();
+//   })
 
-  await test.step('Add product to cart and checkout', async () => {
-   await allPages.cartPage.clickOnCartIcon();
-    await allPages.cartPage.verifyCartItemVisible(productName);
-    await allPages.cartPage.clickOnCheckoutButton();
-  })
+//   await test.step('Add product to cart and checkout', async () => {
+//    await allPages.cartPage.clickOnCartIcon();
+//     await allPages.cartPage.verifyCartItemVisible(productName);
+//     await allPages.cartPage.clickOnCheckoutButton();
+//   })
 
-  await test.step('Place order and click on continue shopping', async () => {
-    await allPages.checkoutPage.verifyCheckoutTitle();
-    await allPages.checkoutPage.verifyProductInCheckout(productName);
-    await allPages.checkoutPage.selectCashOnDelivery();
-    await allPages.checkoutPage.verifyCashOnDeliverySelected();
-    await allPages.checkoutPage.clickOnPlaceOrder();
-    await allPages.checkoutPage.verifyOrderPlacedSuccessfully();
-    await allPages.checkoutPage.verifyOrderItemName(productName);
-    await allPages.inventoryPage.clickOnContinueShopping();
-  })
+//   await test.step('Place order and click on continue shopping', async () => {
+//     await allPages.checkoutPage.verifyCheckoutTitle();
+//     await allPages.checkoutPage.verifyProductInCheckout(productName);
+//     await allPages.checkoutPage.selectCashOnDelivery();
+//     await allPages.checkoutPage.verifyCashOnDeliverySelected();
+//     await allPages.checkoutPage.clickOnPlaceOrder();
+//     await allPages.checkoutPage.verifyOrderPlacedSuccessfully();
+//     await allPages.checkoutPage.verifyOrderItemName(productName);
+//     await allPages.inventoryPage.clickOnContinueShopping();
+//   })
 
-  await test.step('Verify order in My Orders', async () => {
-    await allPages.loginPage.clickOnUserProfileIcon();
-    await allPages.orderPage.clickOnMyOrdersTab();
-    await allPages.orderPage.verifyMyOrdersTitle();
-    await allPages.orderPage.clickOnPaginationButton(2);
-    await allPages.orderPage.verifyProductInOrderList(productName);
-    await allPages.orderPage.verifyPriceAndQuantityInOrderList(productPriceAndQuantity);
-    await allPages.orderPage.verifyOrderStatusInList(orderStatusProcessing, productName);
-    await allPages.orderPage.clickOnPaginationButton(1);
-    await allPages.orderPage.clickViewDetailsButton(1);
-    await allPages.orderPage.verifyOrderDetailsTitle();
-    await allPages.orderPage.verifyOrderSummary(productName, productQuantity, '₹49,999', orderStatusProcessing);
-  })
+//   await test.step('Verify order in My Orders', async () => {
+//     await allPages.loginPage.clickOnUserProfileIcon();
+//     await allPages.orderPage.clickOnMyOrdersTab();
+//     await allPages.orderPage.verifyMyOrdersTitle();
+//     await allPages.orderPage.clickOnPaginationButton(2);
+//     await allPages.orderPage.verifyProductInOrderList(productName);
+//     await allPages.orderPage.verifyPriceAndQuantityInOrderList(productPriceAndQuantity);
+//     await allPages.orderPage.verifyOrderStatusInList(orderStatusProcessing, productName);
+//     await allPages.orderPage.clickOnPaginationButton(1);
+//     await allPages.orderPage.clickViewDetailsButton(1);
+//     await allPages.orderPage.verifyOrderDetailsTitle();
+//     await allPages.orderPage.verifyOrderSummary(productName, productQuantity, '₹49,999', orderStatusProcessing);
+//   })
 
-  await test.step('Cancel order and verify status is updated to Canceled', async () => {
-    await allPages.orderPage.clickCancelOrderButton(2);
-    await allPages.orderPage.confirmCancellation();
-    await allPages.orderPage.verifyCancellationConfirmationMessage();
-    await allPages.orderPage.verifyMyOrdersCount();
-    await allPages.orderPage.clickOnMyOrdersTab();
-    await allPages.orderPage.verifyMyOrdersTitle();
-    await allPages.orderPage.clickOnPaginationButton(2);
-    await allPages.orderPage.verifyOrderStatusInList(orderStatusCanceled, productName);
-  })
-});
+//   await test.step('Cancel order and verify status is updated to Canceled', async () => {
+//     await allPages.orderPage.clickCancelOrderButton(2);
+//     await allPages.orderPage.confirmCancellation();
+//     await allPages.orderPage.verifyCancellationConfirmationMessage();
+//     await allPages.orderPage.verifyMyOrdersCount();
+//     await allPages.orderPage.clickOnMyOrdersTab();
+//     await allPages.orderPage.verifyMyOrdersTitle();
+//     await allPages.orderPage.clickOnPaginationButton(2);
+//     await allPages.orderPage.verifyOrderStatusInList(orderStatusCanceled, productName);
+//   })
+// });
 
 test('Verify that a New User Can Successfully Complete the Journey from Registration to a Single Order Placement', async () => {
   // fresh test data
@@ -397,69 +397,69 @@ test('Verify new user views and cancels an order in my orders', async () => {
   });
 });
 
-test.skip('Verify That a New User Can Successfully Complete the Journey from Registration to a Multiple Order Placement', async () => {
-    const email = `test+${Date.now()}@test.com`;
-    const firstName = 'Test';
-    const lastName = 'User';
+// test.skip('Verify That a New User Can Successfully Complete the Journey from Registration to a Multiple Order Placement', async () => {
+//     const email = `test+${Date.now()}@test.com`;
+//     const firstName = 'Test';
+//     const lastName = 'User';
 
-    let productName= `Rode NT1-A Condenser Mic`;
+//     let productName= `Rode NT1-A Condenser Mic`;
 
-  await test.step('Verify that user can register successfully', async () => {
-    // Signup
-    await allPages.loginPage.clickOnUserProfileIcon();
-    await allPages.loginPage.validateSignInPage();
-    await allPages.loginPage.clickOnSignupLink();
-    await allPages.signupPage.assertSignupPage();
-    await allPages.signupPage.signup(firstName, lastName, email, process.env.PASSWORD);
-    await allPages.signupPage.verifySuccessSignUp();
-  })
+//   await test.step('Verify that user can register successfully', async () => {
+//     // Signup
+//     await allPages.loginPage.clickOnUserProfileIcon();
+//     await allPages.loginPage.validateSignInPage();
+//     await allPages.loginPage.clickOnSignupLink();
+//     await allPages.signupPage.assertSignupPage();
+//     await allPages.signupPage.signup(firstName, lastName, email, process.env.PASSWORD);
+//     await allPages.signupPage.verifySuccessSignUp();
+//   })
 
-  await test.step('Verify that user can login successfully', async () => {
-    // Login as new user
-    await allPages.loginPage.validateSignInPage();
-    await allPages.loginPage.login(email, process.env.PASSWORD);
-    await allPages.loginPage.verifySuccessSignIn();
-    await expect(allPages.homePage.getHomeNav()).toBeVisible({ timeout: 30000 });
-  })
+//   await test.step('Verify that user can login successfully', async () => {
+//     // Login as new user
+//     await allPages.loginPage.validateSignInPage();
+//     await allPages.loginPage.login(email, process.env.PASSWORD);
+//     await allPages.loginPage.verifySuccessSignIn();
+//     await expect(allPages.homePage.getHomeNav()).toBeVisible({ timeout: 30000 });
+//   })
 
-  await test.step('Navigate to All Products and add view details of a random product', async () => {
-    await allPages.homePage.clickOnShopNowButton();
-    await allPages.allProductsPage.assertAllProductsTitle();
-    await allPages.allProductsPage.clickNthProduct(1);
-    await allPages.productDetailsPage.clickOnReviewsTab();
-    await allPages.productDetailsPage.assertReviewsTab();
-    await allPages.productDetailsPage.clickOnAdditionalInfoTab();
-    await allPages.productDetailsPage.assertAdditionalInfoTab();
-  })
+//   await test.step('Navigate to All Products and add view details of a random product', async () => {
+//     await allPages.homePage.clickOnShopNowButton();
+//     await allPages.allProductsPage.assertAllProductsTitle();
+//     await allPages.allProductsPage.clickNthProduct(1);
+//     await allPages.productDetailsPage.clickOnReviewsTab();
+//     await allPages.productDetailsPage.assertReviewsTab();
+//     await allPages.productDetailsPage.clickOnAdditionalInfoTab();
+//     await allPages.productDetailsPage.assertAdditionalInfoTab();
+//   })
 
-  await test.step('Add product to cart, change quantity, add new address and checkout', async () => {
-    await allPages.productDetailsPage.clickAddToCartButton();
-    await allPages.productDetailsPage.clickCartIcon();
-    await allPages.cartPage.clickIncreaseQuantityButton();
-    await allPages.cartPage.clickOnCheckoutButton();
-    await allPages.checkoutPage.verifyCheckoutTitle();
-    await allPages.checkoutPage.selectCashOnDelivery();
-    await allPages.checkoutPage.verifyCashOnDeliverySelected();
-    await allPages.checkoutPage.fillShippingAddress(process.env.SFIRST_NAME, email, process.env.SCITY, process.env.SSTATE, process.env.SSTREET_ADD, process.env.SZIP_CODE, process.env.SCOUNTRY);
-    await allPages.checkoutPage.clickSaveAddressButton();
-    await allPages.checkoutPage.clickOnPlaceOrder();
-    await allPages.checkoutPage.verifyOrderPlacedSuccessfully();
-    await allPages.checkoutPage.verifyOrderConfirmedTitle();
-    await allPages.checkoutPage.clickOnContinueShoppingButton();
-  })
+//   await test.step('Add product to cart, change quantity, add new address and checkout', async () => {
+//     await allPages.productDetailsPage.clickAddToCartButton();
+//     await allPages.productDetailsPage.clickCartIcon();
+//     await allPages.cartPage.clickIncreaseQuantityButton();
+//     await allPages.cartPage.clickOnCheckoutButton();
+//     await allPages.checkoutPage.verifyCheckoutTitle();
+//     await allPages.checkoutPage.selectCashOnDelivery();
+//     await allPages.checkoutPage.verifyCashOnDeliverySelected();
+//     await allPages.checkoutPage.fillShippingAddress(process.env.SFIRST_NAME, email, process.env.SCITY, process.env.SSTATE, process.env.SSTREET_ADD, process.env.SZIP_CODE, process.env.SCOUNTRY);
+//     await allPages.checkoutPage.clickSaveAddressButton();
+//     await allPages.checkoutPage.clickOnPlaceOrder();
+//     await allPages.checkoutPage.verifyOrderPlacedSuccessfully();
+//     await allPages.checkoutPage.verifyOrderConfirmedTitle();
+//     await allPages.checkoutPage.clickOnContinueShoppingButton();
+//   })
 
-  await test.step('Add another product to cart, select existing address and checkout', async () => {
-    await allPages.homePage.clickOnShopNowButton();
-    await allPages.allProductsPage.assertAllProductsTitle();
-    await allPages.allProductsPage.clickNthProduct(1);
-    await allPages.productDetailsPage.clickAddToCartButton();
-    await allPages.productDetailsPage.clickCartIcon();
-    await allPages.cartPage.clickOnCheckoutButton();
-    await allPages.checkoutPage.verifyCheckoutTitle();
-    await allPages.checkoutPage.selectCashOnDelivery();
-    await allPages.checkoutPage.verifyCashOnDeliverySelected();
-    await allPages.checkoutPage.clickOnPlaceOrder();
-    await allPages.checkoutPage.verifyOrderPlacedSuccessfully();
-  })
-});
+//   await test.step('Add another product to cart, select existing address and checkout', async () => {
+//     await allPages.homePage.clickOnShopNowButton();
+//     await allPages.allProductsPage.assertAllProductsTitle();
+//     await allPages.allProductsPage.clickNthProduct(1);
+//     await allPages.productDetailsPage.clickAddToCartButton();
+//     await allPages.productDetailsPage.clickCartIcon();
+//     await allPages.cartPage.clickOnCheckoutButton();
+//     await allPages.checkoutPage.verifyCheckoutTitle();
+//     await allPages.checkoutPage.selectCashOnDelivery();
+//     await allPages.checkoutPage.verifyCashOnDeliverySelected();
+//     await allPages.checkoutPage.clickOnPlaceOrder();
+//     await allPages.checkoutPage.verifyOrderPlacedSuccessfully();
+//   })
+// });
 

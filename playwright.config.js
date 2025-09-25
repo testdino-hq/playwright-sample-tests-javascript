1a0bffd57ff7971f3c6e1e0520105626c98467f8
testing <testing@gmail.com>
2025-09-25 15:21:28 +0530

    Updated the test cases and added Readme file

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
 
   use: {
     baseURL: 'https://demo.alphabin.co/',
-    headless: false,
+    headless: true,
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',

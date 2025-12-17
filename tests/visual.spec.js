bdb29bd0a39eac44dcfcdc559a919c2978f1b4af
kriti2710 <kriti@testdino.com>
2025-12-17 17:51:26 +0530

    Add '@api' to Playwright test grep pattern

--- tests/visual.spec.js
+++ tests/visual.spec.js
@@ -12,9 +12,15 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.skip('test', {tag: '@chromium'}, async ({ page }) => {
+  await test.step('Test visual comparison', async () => {
   await page.goto('https://github.com/login');
-  await expect(page).toHaveScreenshot('github-login.png');
+  });
+  await test.step('Verify that it is the same as the baseline image', async () => {
+    await expect(page).toHaveScreenshot('github-login.png');
+  });
+  await test.step('Verify visual comparison', async () => {
   await page.getByRole('textbox', { name: 'Username or email address' }).click();
   await page.getByRole('textbox', { name: 'Username or email address' }).fill('test');
   await expect(page).toHaveScreenshot('github-login.png');
+  });
 });

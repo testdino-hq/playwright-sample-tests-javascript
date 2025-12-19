3ff1311cc4cdb62e96338d89e9a70eb7d662390b
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2025-12-19 11:43:24 +0530

    Merge branch 'main' into staging

--- tests/cart_checkout.spec.js
+++ tests/cart_checkout.spec.js
@@ -16,7 +16,6 @@ async function login(username = process.env.USERNAME, password = process.env.PAS
   await allPages.loginPage.validateSignInPage();
   await allPages.loginPage.login(username, password);
 }
-
 async function login1(username = process.env.USERNAME1, password = process.env.PASSWORD) {
   await allPages.loginPage.clickOnUserProfileIcon();
   await allPages.loginPage.validateSignInPage();

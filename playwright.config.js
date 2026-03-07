89692a31cc7828afeeec1d7129f36c2ec3c12176
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2026-03-07 20:25:54 +0530

    Refactor Playwright config for improved reporting

--- playwright.config.js
+++ playwright.config.js
@@ -3,29 +3,33 @@ import { defineConfig, devices } from '@playwright/test';
 import * as dotenv from 'dotenv';
 
 dotenv.config({ quiet: true });
+
 const isCI = !!process.env.CI;
 
 export default defineConfig({
   testDir: './tests',
+
   fullyParallel: true,
   forbidOnly: isCI,
+
   retries: isCI ? 0 : 0,
   workers: isCI ? 1 : 1,
-  
 
   timeout: 30 * 1000,
+
   reporter: [
+    ['blob', { outputDir: 'blob-report' }],   // Required for shard merging
+    ['json', { outputFile: './playwright-report/report.json' }], // Required for TestDino
     ['html', {
       outputFolder: 'playwright-report',
       open: 'never'
-    }],
-    ['blob', { outputDir: 'blob-report' }], // Blob reporter for merging
-    ['json', { outputFile: './playwright-report/report.json' }],
+    }]
   ],
 
   use: {
     baseURL: 'https://storedemo.testdino.com',
     headless: true,
+
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
@@ -35,17 +39,17 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-      grep: /@chromium/, // only run tests tagged @chromium
+      grep: /@chromium/,
     },
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
-      grep: /@firefox/, // only run tests tagged @firefox
+      grep: /@firefox/,
     },
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-      grep: /@webkit/, // only run tests tagged @webkit
+      grep: /@webkit/,
     },
   ],
 });
